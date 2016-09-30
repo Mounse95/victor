ef78e7048c212b9ab29ab849e744b86fd7db07a3
Monisha Rambhujun <Monisha Rambhujun>
2016-09-30 02:15:08 -0400

    ajout de projet 1

---
+++ Project1/List.java
@@ -0,0 +1,32 @@
+package Project1;
+
+public class List {
+
+	public List() {
+		// Default method, will make it 10 itemps when no parameters
+
+	}
+
+	public List(int number) {
+		// Set the number of items in the list
+	}
+
+	private void retainState() {
+		// save to file
+	}
+
+	public void addFood(String foodName, int calories) {
+		// add food and calories to list
+
+	}
+
+	public void sort() {
+		// list food in descending order
+
+	}
+
+	public void viewAndIterate() {
+
+	}
+
+}
---
+++ Project1/testcode.java
@@ -0,0 +1,16 @@
+package Project1;
+
+import java.util.*;
+public class testcode {
+	 private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args){
+		System.out.println("How many items do you want to input?");
+		List list = new List();
+		List list1 = new List();
+		
+		
+	}
+	
+}
+	
